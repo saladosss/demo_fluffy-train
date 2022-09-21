45ce0b4ca4144418f1cc3235bd4c6680fbbf257b
Artem Mukhin <amuhin310@gmail.com>
2022-09-21 21:08:45 +0300

    SFMl include

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/VideoMode.hpp>
 
-using namespace std;
+using namespace sf;
 
 int main(){
-    cout << "Hello"s << endl;
+    RenderWindow window(VideoMode(800, 600), "SFML");
+
+    while (window.isOpen()) {
+    
+        Event event;
+        while(window.pollEvent(event)){
+            if(event.type == Event::Closed){
+                window.close();
+            }
+            
+        }
+        window.clear(Color(0, 0, 0, 0));
+
+        CircleShape triangle(80, 3);
+        triangle.setFillColor(Color(255, 255, 255));
+
+    window.draw(triangle);
+
+    window.display();
+    }
     return 0;
 }
